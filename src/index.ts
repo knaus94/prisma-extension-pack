6ab0cb869abbff085206449549be3ffe0b3713d4
knaus94 <mihitovdima@gmail.com>
2024-06-18 10:47:14 +0300

    chore: Update TypeScript and Prisma dependencies
    This commit updates the TypeScript and Prisma dependencies in the `tsconfig.json` and `package.json` files. The TypeScript dependency is updated from version 5.2.2 to 5.15.0, and the Prisma dependency is updated from version 5.3.1 to 5.15.0.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,107 @@
-import { Prisma } from "@prisma/client/extension";
+import { Prisma as PrismaExtension } from "@prisma/client/extension";
+import { backOff, IBackOffOptions } from "exponential-backoff";
+import { Prisma } from "@prisma/client";
+
+type FlatTransactionClient = Prisma.TransactionClient & {
+  $commit: () => Promise<void>;
+  $rollback: () => Promise<void>;
+};
+
+const ROLLBACK = { [Symbol.for("prisma.client.extension.rollback")]: true };
 
 type Pagination = {
   take?: number;
   skip?: number;
 };
 
+export function RetryTransactions(options?: Partial<IBackOffOptions>) {
+  return Prisma.defineExtension((prisma) =>
+    prisma.$extends({
+      client: {
+        $transaction(...args: any) {
+          return backOff(() => prisma.$transaction.apply(prisma, args), {
+            retry: (e) => {
+              // Retry the transaction only if the error was due to a write conflict or deadlock
+              // See: https://www.prisma.io/docs/reference/api-reference/error-reference#p2034
+              return e?.code === "P2034";
+            },
+            ...options,
+          });
+        },
+      } as { $transaction: (typeof prisma)["$transaction"] },
+    })
+  );
+}
+
 export default () => {
-  return Prisma.defineExtension({
+  return PrismaExtension.defineExtension({
     name: "pack",
+    client: {
+      async $begin() {
+        const prisma = Prisma.getExtensionContext(this);
+        let setTxClient: (txClient: Prisma.TransactionClient) => void;
+        let commit: () => void;
+        let rollback: () => void;
+
+        // a promise for getting the tx inner client
+        const txClient = new Promise<Prisma.TransactionClient>((res) => {
+          setTxClient = (txClient) => res(txClient);
+        });
+
+        // a promise for controlling the transaction
+        const txPromise = new Promise((_res, _rej) => {
+          commit = () => _res(undefined);
+          rollback = () => _rej(ROLLBACK);
+        });
+
+        // opening a transaction to control externally
+        if (
+          "$transaction" in prisma &&
+          typeof prisma.$transaction === "function"
+        ) {
+          const tx = prisma.$transaction((txClient: any) => {
+            setTxClient(txClient as unknown as Prisma.TransactionClient);
+
+            return txPromise.catch((e) => {
+              if (e === ROLLBACK) return;
+              throw e;
+            });
+          });
+
+          // return a proxy TransactionClient with `$commit` and `$rollback` methods
+          return new Proxy(await txClient, {
+            get(target, prop) {
+              if (prop === "$commit") {
+                return () => {
+                  commit();
+                  return tx;
+                };
+              }
+              if (prop === "$rollback") {
+                return () => {
+                  rollback();
+                  return tx;
+                };
+              }
+              return target[prop as keyof typeof target];
+            },
+          }) as FlatTransactionClient;
+        }
+
+        throw new Error("Transactions are not supported by this client");
+      },
+    },
     model: {
       $allModels: {
         async findRandom<T, A>(
           this: T,
-          args?: Prisma.Exact<A, Prisma.Args<T, "findFirst">> & object
+          args?: PrismaExtension.Exact<
+            A,
+            PrismaExtension.Args<T, "findFirst">
+          > &
+            object
         ) {
-          const context = Prisma.getExtensionContext(this);
+          const context = PrismaExtension.getExtensionContext(this);
 
           const numRows = (await (context as any).count({
             where: (args as { where?: object } | undefined)?.where,
@@ -22,22 +109,25 @@ export default () => {
           return (await (context as any).findFirst({
             ...args,
             skip: Math.max(0, Math.floor(Math.random() * numRows)),
-          })) as Prisma.Result<T, A, "findFirst">;
+          })) as PrismaExtension.Result<T, A, "findFirst">;
         },
 
         async findManyRandom<T, TWhere, TSelect>(
           this: T,
           num: number,
           args?: {
-            where?: Prisma.Exact<TWhere, Prisma.Args<T, "findFirst">["where"]>;
-            select?: Prisma.Exact<
+            where?: PrismaExtension.Exact<
+              TWhere,
+              PrismaExtension.Args<T, "findFirst">["where"]
+            >;
+            select?: PrismaExtension.Exact<
               TSelect,
-              Prisma.Args<T, "findFirst">["select"] & { id: true }
+              PrismaExtension.Args<T, "findFirst">["select"] & { id: true }
             >;
           }
         ) {
-          const context = Prisma.getExtensionContext(this);
-          type FindFirstResult = Prisma.Result<
+          const context = PrismaExtension.getExtensionContext(this);
+          type FindFirstResult = PrismaExtension.Result<
             T,
             { where: TWhere; select: TSelect },
             "findFirst"
@@ -79,9 +169,9 @@ export default () => {
 
         async exists<T>(
           this: T,
-          where: Prisma.Args<T, "findFirst">["where"]
+          where: PrismaExtension.Args<T, "findFirst">["where"]
         ): Promise<boolean> {
-          const context = Prisma.getExtensionContext(this);
+          const context = PrismaExtension.getExtensionContext(this);
 
           const result = await (context as any).findFirst({ where });
           return result !== null;
@@ -89,29 +179,62 @@ export default () => {
 
         async paginate<T, A>(
           this: T,
-          args?: Prisma.Exact<A, Prisma.Args<T, "findMany">> & {
+          args?: PrismaExtension.Exact<
+            A,
+            PrismaExtension.Args<T, "findMany">
+          > & {
             pagination?: Pagination;
           }
         ) {
-          const context = Prisma.getExtensionContext(this);
+          const context = PrismaExtension.getExtensionContext(this);
           const { pagination, ...operationArgs } = (args ?? {}) as any;
           const take = args?.pagination?.take ?? operationArgs?.take ?? 10;
           const skip = args?.pagination?.skip ?? operationArgs?.skip ?? 0;
 
-          const [data, total]: [Prisma.Result<T, A, "findMany">, number] =
-            await Promise.all([
-              (context as any).findMany({
-                ...operationArgs,
-                skip,
-                take,
-              }),
-              (context as any).count({ where: operationArgs?.where }),
-            ]);
+          const [data, total]: [
+            PrismaExtension.Result<T, A, "findMany">,
+            number
+          ] = await Promise.all([
+            (context as any).findMany({
+              ...operationArgs,
+              skip,
+              take,
+            }),
+            (context as any).count({ where: operationArgs?.where }),
+          ]);
 
           return {
             data,
             total,
           };
+        },
+        async updateIgnoreOnNotFound<T, A>(
+          this: T,
+          args: PrismaExtension.Exact<A, PrismaExtension.Args<T, "update">>
+        ): Promise<PrismaExtension.Result<T, A, "update"> | null> {
+          try {
+            const context = PrismaExtension.getExtensionContext(this) as any;
+            return await context.update(args);
+          } catch (err) {
+            if (err?.code === "P2025") {
+              return null;
+            }
+            throw err;
+          }
+        },
+        async deleteIgnoreOnNotFound<T, A>(
+          this: T,
+          args: PrismaExtension.Exact<A, PrismaExtension.Args<T, "delete">>
+        ): Promise<PrismaExtension.Result<T, A, "delete"> | null> {
+          try {
+            const context = PrismaExtension.getExtensionContext(this) as any;
+            return await context.delete(args);
+          } catch (err) {
+            if (err?.code === "P2025") {
+              return null;
+            }
+            throw err;
+          }
         },
       },
     },
